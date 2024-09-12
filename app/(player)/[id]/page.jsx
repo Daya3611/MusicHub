6cb27dfaa4a4c1d3442e88027c2649affb22e124
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-09-12 21:04:41 +0530

    Upadte 1.0.2

--- app/(player)/[id]/page.jsx
+++ app/(player)/[id]/page.jsx
@@ -1,19 +1,21 @@
 import { getSongsById } from "@/lib/fetch";
 import Player from "../_components/Player";
 import Recomandation from "../_components/Recomandation";
+import AdvanceSearch from "../_components/AdvanceSearch";
 
 export const generateMetadata = async ({ params }) => {
     const title = await getSongsById(params.id);
     const data = await title.json();
     return {
-        title: `${data.song}`
+        title: `Now Playing - ${data.data[0].name}`
     }
 }
 
 export default function Page({ params }) {
     return (
         <div>
-            <Player params={params} />
+            <AdvanceSearch/>
+            <Player id={params.id} />
             <Recomandation id={params.id} />
         </div>
     )
--- app/(player)/_components/Player.jsx
+++ app/(player)/_components/Player.jsx
@@ -1,29 +1,36 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { getSongsById, getSongsLyricsById } from "@/lib/fetch";
-import { Download, Pause, Play, RedoDot, UndoDot, Repeat, Loader2, Bookmark, BookmarkCheck } from "lucide-react";
+import { Download, Pause, Play, RedoDot, UndoDot, Repeat, Loader2, Bookmark, BookmarkCheck, Repeat1 } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { Slider } from "@/components/ui/slider";
 import { toast } from "sonner";
 
-export default function Player({ params }) {
+export default function Player({ id }) {
     const [data, setData] = useState([]);
-    const [playing, setPlaying] = useState(false);
+    const [playing, setPlaying] = useState(true);
     const audioRef = useRef(null);
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
     const [isDownloading, setIsDownloading] = useState(false);
     const [isLooping, setIsLooping] = useState(false);
     const [isSaved, setIsSaved] = useState(false);
+    const [audioURL, setAudioURL] = useState("");
 
     const getSong = async () => {
-        const get = await getSongsById(params.id);
+        const get = await getSongsById(id);
         const data = await get.json();
-        setData(data);
+        setData(data.data[0]);
+        if (data?.data[0]?.downloadUrl[2]?.url) {
+            setAudioURL(data?.data[0]?.downloadUrl[2]?.url);
+        } else if (data?.data[0]?.downloadUrl[1]?.url) {
+            setAudioURL(data?.data[0]?.downloadUrl[1]?.url);
+        } else {
+            setAudioURL(data?.data[0]?.downloadUrl[0]?.url);
+        }
     };
-
 
     const formatTime = (time) => {
         const minutes = Math.floor(time / 60);
@@ -42,12 +49,12 @@ export default function Player({ params }) {
 
     const downloadSong = async () => {
         setIsDownloading(true);
-        const response = await fetch(data.media_url);
+        const response = await fetch(audioURL);
         const datas = await response.blob();
         const url = URL.createObjectURL(datas);
         const a = document.createElement('a');
         a.href = url;
-        a.download = `${data.song}.mp3`;
+        a.download = `${data.name}.mp3`;
         a.click();
         URL.revokeObjectURL(url);
         toast.success('downloaded');
@@ -72,20 +79,20 @@ export default function Player({ params }) {
 
     const handleAddToBookmark = () => {
         let exisn = localStorage.getItem("saved");
-        if (exisn != null && exisn.split(" ").find(e => e == params.id)) {
-            localStorage.setItem("saved", exisn.split(" ").filter(e => e != params.id).join(" "));
+        if (exisn != null && exisn.split(" ").find(e => e == id)) {
+            localStorage.setItem("saved", exisn.split(" ").filter(e => e != id).join(" "));
             setIsSaved(false);
             return toast.success('Removed from Bookmarks!');
         }
         setIsSaved(true);
-        localStorage.setItem("saved", `${exisn != null ? exisn : ""} ${params.id}`);
+        localStorage.setItem("saved", `${exisn != null ? exisn : ""} ${id}`);
         toast.success('Saved to Bookmarks!');
     };
 
     useEffect(() => {
         getSong();
         let exisn = localStorage.getItem("saved");
-        if (exisn != null && exisn.split(" ").find(e => e == params.id)) {
+        if (exisn != null && exisn.split(" ").find(e => e == id)) {
             setIsSaved(true);
         }
         const handleTimeUpdate = () => {
@@ -105,18 +112,18 @@ export default function Player({ params }) {
         };
     }, []);
     return (
-        <div className="mb-3 mt-5 px-6 md:px-20 lg:px-32">
-            <audio onPlay={() => setPlaying(true)} onPause={() => setPlaying(false)} onLoadedData={() => setDuration(audioRef.current.duration)} src={data.media_url} ref={audioRef}></audio>
+        <div className="mb-3 mt-5">
+            <audio onPlay={() => setPlaying(true)} onPause={() => setPlaying(false)} onLoadedData={() => setDuration(audioRef.current.duration)} autoPlay={playing} src={audioURL} ref={audioRef}></audio>
             <div className="grid gap-6 w-full">
-                <div className="sm:flex grid gap-5 w-full">
+                <div className="sm:flex px-6 md:px-20 lg:px-32 grid gap-5 w-full">
                     <div>
-                        {!data.image ? (
-                            <Skeleton className="md:w-[130px] rounded-2xl md:h-[150px] w-full h-[400px]" />
+                        {data.length <= 0 ? (
+                            <Skeleton className="md:w-[130px] rounded-2xl md:h-[150px] w-full h-[370px]" />
                         ) : (
-                            <img src={data.image} className="sm:h-[150px] rounded-2xl sm:w-[200px] w-full object-cover" />
+                            <img src={data.image[2].url} className="sm:h-[150px] h-full bg-secondary/50 rounded-2xl sm:w-[200px] w-full object-cover" />
                         )}
                     </div>
-                    {!data.song ? (
+                    {data.length <= 0 ? (
                         <div className="flex flex-col justify-between w-full">
                             <div>
                                 <Skeleton className="h-4 w-36 mb-2" />
@@ -138,21 +145,21 @@ export default function Player({ params }) {
                     ) : (
                         <div className="flex flex-col justify-between w-full">
                             <div>
-                                <h1 className="text-xl font-bold md:max-w-lg max-w-[260px]">{data.song}</h1>
-                                <p className="text-xs text-muted-foreground">{data.singers || "unknown"}</p>
+                                <h1 className="text-xl font-bold md:max-w-lg max-w-[260px]">{data.name}</h1>
+                                <p className="text-xs text-muted-foreground">{data.artists.primary[0]?.name || "unknown"}</p>
                             </div>
                             <div className="grid gap-2 w-full mt-5 sm:mt-0">
-                                <Slider onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full md:max-w-[400px]" />
-                                <div className="w-full md:max-w-[400px] flex items-center justify-between">
+                                <Slider onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full" />
+                                <div className="w-full flex items-center justify-between">
                                     <span className="text-xs">{formatTime(currentTime)}</span>
                                     <span className="text-xs">{formatTime(duration)}</span>
                                 </div>
-                                <div className="flex items-center justify-between mt-2 md:max-w-[400px]">
+                                <div className="flex items-center justify-between mt-2">
                                     <div className="flex items-center gap-3 sm:mt-0">
-                                        <Button size="icon" variant={isLooping ? "secondary" : "outline"} onClick={loopSong}>
-                                            <Repeat className="h-4 w-4" />
+                                        <Button size="icon" variant="outline" onClick={loopSong}>
+                                            {!isLooping ? <Repeat className="h-4 w-4" /> : <Repeat1 className="h-4 w-4" />}
                                         </Button>
-                                        <Button size="icon" onClick={togglePlayPause}>
+                                        <Button size="icon" variant={playing ? "gooeyRight" : "gooeyLeft"} onClick={togglePlayPause}>
                                             {playing ? (
                                                 <Pause className="h-4 w-4" />
                                             ) : (
