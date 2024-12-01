c8d31c3651918edd092089a41ea7b29bd4ecef27
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-12-01 14:10:23 +0530

    add logo to footer

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import Logo from "./logo";
 
 export default function Footer() {
     return (
         <footer className="py-5 backdrop-blur-3xl mt-8 px-6 md:px-20 lg:px-32">
-            {/* <div>
-                <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
-            </div> */}
+            <div className="text-center">
+                {/* <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1> */}
+                <Logo/>
+                <p className="text-center text-muted-foreground text-sm mt-2  font-light">I dont own the api it belongs to its respective owners  jiosaavn.com  , built for educational purposes.</p>
+            </div>
             
-            <p className="text-center text-muted-foreground text-sm mt-2 max-w-lg font-light">I dont own the api it belongs to its respective owners  jiosaavn.com  , built for educational purposes.</p>
+            
             <p className="text-center text-sm text-muted-foreground">Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://github.com/Daya3611">Daya3611</a>.</p>
             <div className="flex gap-3 items-center justify-center mt-3">
                 
